12f3ab9dd97df80c6af0b5aeace3d8399970c4aa
Goddo-ro <r0manovic1@yandex.ru>
2024-11-01 21:51:42 +0300

    refactor: make form flex with gap

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,4 +1,10 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { BaseQueryFn } from '@reduxjs/toolkit/query';
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from 'axios';
 
 export const API_URL = 'http://127.0.0.1:8080/api/';
 axios.defaults.withCredentials = true;
