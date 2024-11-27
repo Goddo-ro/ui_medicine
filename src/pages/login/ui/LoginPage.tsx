15aff392f74762d382db1711ddb38b933e9ac480
Goddo-ro <r0manovic1@yandex.ru>
2024-11-27 12:05:37 +0300

    add: firebase auth

--- src/pages/login/ui/LoginPage.tsx
+++ src/pages/login/ui/LoginPage.tsx
@@ -1,16 +1,37 @@
 import { loginThunk, selectAuth } from '@/entities/viewer';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import { useState } from 'react';
+import { StyledFirebaseAuth } from 'react-firebaseui';
 import { Navigate } from 'react-router-dom';
 
 import { loginData } from '@/pages/login/model/login-schema';
 
 import { useAppDispatch, useAppSelector } from '@/shared/lib/store';
 import { paths } from '@/shared/routes/routes';
-import { IFieldError } from '@/shared/types/fieldError';
 import { Button } from '@/shared/ui/button/Button';
 import { Form } from '@/shared/ui/form/Form';
 import { Input } from '@/shared/ui/input/Input';
-import { errorParser } from '@/shared/zod/errorParser';
+import { IFieldError, errorParser } from '@/shared/zod/errorParser';
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  // ...
+};
+firebase.initializeApp(config);
+
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+  ],
+};
 
 export const LoginPage = () => {
   const [errors, setErrors] = useState<IFieldError[]>([]);
@@ -44,32 +65,36 @@ export const LoginPage = () => {
   // TODO: loader while auth checking
 
   return (
-    <Form
-      onSubmit={(e) => {
-        e.preventDefault();
-        validate(new FormData(e.currentTarget));
-      }}
-      title='Вход в аккаунт'
-    >
-      <Input
-        type='input'
-        label='Почта'
-        id='email'
-        name='email'
-        required
-        error={errors.find((error) => error.fieldName === 'email')?.message}
-      />
-
-      <Input
-        label='Пароль'
-        type='password'
-        id='password'
-        name='password'
-        error={errors.find((error) => error.fieldName === 'password')?.message}
-        required
-      />
-
-      <Button type='submit'>Войти</Button>
-    </Form>
+    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
   );
+
+  // return (
+  //   <Form
+  //     onSubmit={(e) => {
+  //       e.preventDefault();
+  //       validate(new FormData(e.currentTarget));
+  //     }}
+  //     title='Вход в аккаунт'
+  //   >
+  //     <Input
+  //       type='input'
+  //       label='Почта'
+  //       id='email'
+  //       name='email'
+  //       required
+  //       error={errors.find((error) => error.fieldName === 'email')?.message}
+  //     />
+
+  //     <Input
+  //       label='Пароль'
+  //       type='password'
+  //       id='password'
+  //       name='password'
+  //       error={errors.find((error) => error.fieldName === 'password')?.message}
+  //       required
+  //     />
+
+  //     <Button type='submit'>Войти</Button>
+  //   </Form>
+  // );
 };
